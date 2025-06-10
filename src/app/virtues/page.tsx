4fc6f67d21eb694ad1e8d47493a8729cda4bb1b2
banhducnonggg <banhducnonggg@gmail.com>
2025-06-10 15:13:07 +0700

    feat: update version to 0.15.3 in package.json and modify text in Virtues component

--- src/app/virtues/page.tsx
+++ src/app/virtues/page.tsx
@@ -7,7 +7,7 @@ export default function Virtues() {
       <div className="flex items-center justify-center w-full min-h-screen bg-lust-accent-light">
         <div className="relative flex flex-col items-center justify-center w-full h-screen bg-lust-accent-dark">
           <MorphingText
-            texts={["I meet this one girl", "I love her so much"]}
+            texts={["I meet this girl", "I love her so much"]}
             className="text-2xl md:text-4xl w-full font-title text-lust-accent-light"
           ></MorphingText>
         </div>
