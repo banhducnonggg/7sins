25f2cb15601f23c3e961c624c313818f8729a6ab
banhducnonggg <banhducnonggg@gmail.com>
2025-06-13 15:09:13 +0700

    feat: enhance site metadata for 1 on 1 Creative Lab by adding robots directives to improve SEO and indexing capabilities. hide the easter egg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -156,6 +156,17 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "1 on 1 Creative Lab",
   },
+  robots: {
+    index: true,
+    follow: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      'max-video-preview': -1,
+      'max-image-preview': 'large',
+      'max-snippet': -1,
+    },
+  },
 };
 
 export default function RootLayout({
