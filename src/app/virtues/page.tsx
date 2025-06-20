425a06088e668d98b09c89a8f269710e403a1076
banhducnonggg <banhducnonggg@gmail.com>
2025-06-20 18:26:46 +0700

    chore: add metadata generation for Virtues page and ensure consistent layout structure

--- src/app/virtues/page.tsx
+++ src/app/virtues/page.tsx
@@ -5,16 +5,37 @@ import VFooter from "@/components/Widgets/virtues/VFooter";
 
 export default function Virtues() {
   return (
-      <div className="flex flex-col items-center justify-center w-full min-h-screen max-w-screen bg-background">
-        <div className="relative z-10 flex flex-col items-center justify-center w-full h-screen bg-lust-accent-dark">
-          <MorphingText
-            texts={["An Easter Egg", "I meet this girl", "I love her so much"]}
-            className="w-full text-2xl md:text-4xl font-title text-lust-accent-light"
-          ></MorphingText>
-        </div>
-        <SubHero />
-        <Floating />
-        <VFooter />
+    <div className="flex flex-col items-center justify-center w-full min-h-screen max-w-screen bg-background">
+      <div className="relative z-10 flex flex-col items-center justify-center w-full h-screen bg-lust-accent-dark">
+        <MorphingText
+          texts={["An Easter Egg", "I meet this girl", "I love her so much"]}
+          className="w-full text-2xl md:text-4xl font-title text-lust-accent-light"
+        ></MorphingText>
       </div>
+      <SubHero />
+      <Floating />
+      <VFooter />
+    </div>
   );
 }
+
+export const generateMetadata = () => ({
+  title: "The Easter Egg",
+  description: "Discover the easter egg.",
+  openGraph: {
+    title: "The Easter Egg",
+    description: "Discover the easter egg.",
+    url: "https://1on1.vercel.app/virtues",
+    siteName: "1 on 1",
+    images: [
+      {
+        url: "/lust/lust.png",
+        width: 1801,
+        height: 1800,
+        alt: "Virtues Open Graph Image",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+});
